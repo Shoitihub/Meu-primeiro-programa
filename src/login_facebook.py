99fe81184fb3cc727edd5199dd035ff253994410
Shoiti <shoiti.isotani@gmail.com>
2023-10-22 09:56:52 -0300

    bug corrigido

--- src/login_facebook.py
+++ src/login_facebook.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
@@ -7,6 +8,13 @@ service = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=service)
 
 navegador.get('https://www.facebook.com/')
-navegador.find_element('xpath', '//*[@id="email"]').send_keys('shoiti.isotani@gmail.com')#usuário
-navegador.find_element('xpath', '//*[@id="pass"]').send_keys('Shoiti2605')#senha
-navegador.find_element('xpath', '//*[@id="u_0_5_v1"]').click()
+# Find the username and password input fields and login button using better locators
+username_field = navegador.find_element(By.ID, 'email')
+password_field = navegador.find_element(By.ID, 'pass')
+login_button = navegador.find_element(By.NAME, 'login')
+
+# Enter the username and password and click the login button
+username_field.send_keys('shoiti.isotani@gmail.com')
+password_field.send_keys('Shoiti2605')
+login_button.click()
+
