621678a4ac69e9c0bbcdfba2829f5ea0dad8bc05
Shoiti <shoiti.isotani@gmail.com>
2023-10-31 18:30:38 -0300

    new version

--- src/facebook.py
+++ src/facebook.py
@@ -13,23 +13,19 @@ options.add_argument('--disable-notifications')
 
 driver = webdriver.Chrome(service=service, options=options)
 
-id = 'shoiti.isotani@gmail.com'
+id = str(input('Digite seu email - '))
 key = getpass.getpass('Digite sua senha - ')
 driver.set_page_load_timeout(10)
 driver.get("https://facebook.com")
 driver.maximize_window()
 driver.implicitly_wait(2)
-driver.find_element(By.ID, "email").send_keys(id)
-driver.find_element(By.ID, "pass").send_keys(key)
+driver.find_element(By.NAME, "email").send_keys(id)
+driver.find_element(By.NAME, "pass").send_keys(key)
 driver.find_element(By.NAME, "login").click()
-driver.implicitly_wait(1)
-if driver.title == "Facebook":
-    driver.get("https://www.facebook.com/events/birthdays")
-    driver.implicitly_wait(5)
-    message = driver.find_elements('xpath', "//textarea[@class='enter_submit uiTextareaNoResize uiTextareaAutogrow uiStreamInlineTextarea inlineReplyTextArea mentionsTextarea textInput']")
-    for text in message:
-        text.send_keys("Feliz aniversário")
-        text.send_keys(Keys.RETURN)
-else:
-    print('Informação inválida. Execute o código novamente')
-    driver.quit()
+driver.implicitly_wait(5)
+driver.get("https://www.facebook.com/events/birthdays")
+driver.implicitly_wait(5)
+message = driver.find_elements('xpath', "//textarea[@class='enter_submit uiTextareaNoResize uiTextareaAutogrow uiStreamInlineTextarea inlineReplyTextArea mentionsTextarea textInput']")
+for text in message:
+    text.send_keys("Feliz aniversário")
+    text.send_keys(Keys.RETURN)
